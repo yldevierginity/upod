86f9935d31e56b92da544f687538f62151dcfd8a
raemoon <carlsuello143@gmail.com>
2025-05-15 10:45:53 +0800

    fix: login transition

--- upodusers/templates/script.js
+++ upodusers/templates/script.js
@@ -2,13 +2,13 @@ const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
 
-// registerBtn.addEventListener('click', () => {
-//     container.classList.add("active");
-// });
+registerBtn.addEventListener('click', () => {
+    container.classList.add('active');
+});
 
-// loginBtn.addEventListener('click', () => {
-//     container.classList.remove("active");
-// });
+loginBtn.addEventListener('click', () => {
+    container.classList.remove('active');
+});
 
 function onLoad() {
   gapi.load('auth2', function() {
