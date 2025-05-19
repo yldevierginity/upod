1d71eceb20538ce66dffda61b10568136c462954
0xshr00msz <yldeviermagpusao@gmail.com>
2025-05-19 21:40:09 +0800

    fix(upodusers): partially fixed the input fields of index

--- upodusers/urls.py
+++ upodusers/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+# Custom URL imports
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('logout', views.logout_view, name='logout'),
