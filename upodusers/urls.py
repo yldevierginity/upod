fd2bdeff410bb89a15cd935f4b5b11d3d3dfbf63
0xshr00msz <yldeviermagpusao@gmail.com>
2025-05-22 03:57:14 +0800

    fix(redirects): resolve django templates css layout and redirects when logging in and logging out

--- upodusers/urls.py
+++ upodusers/urls.py
@@ -4,6 +4,9 @@ from . import views
 # Custom URL imports
 
 urlpatterns = [
+    # This is the app default index view url
     path('', views.index, name='index'),
+    # This path is used when resetting password so that it does not redirect to the allauth default login template
+    path('/', views.index, name='root'),
     path('logout', views.logout_view, name='logout'),
 ]
