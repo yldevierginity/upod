d73356981a7b77e73ce5cb217521da21557158bc
0xshr00msz <yldeviermagpusao@gmail.com>
2025-05-29 00:09:58 +0800

    chore(directory): remove unused initialized directories

--- userdashboard/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.contrib.auth.views import LogoutView
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('accounts/logout/', LogoutView.as_view(), name="account_logout"),
-    path('logout', views.logout_views, name='logout_views'),
-]
